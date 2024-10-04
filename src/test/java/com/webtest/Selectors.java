c1709c4d11a968d8c2cf55f4b906209ad020db68
lucas <lucassilva.uern@gmail.com>
2024-10-04 17:19:13 -0300

    feat: criação de constantes

---
+++ src/test/java/com/webtest/Selectors.java
@@ -0,0 +1,20 @@
+package com.webtest;
+
+public class Selectors {
+    public static final String INPUT_FIRST_NAME = "input[placeholder='First Name']";
+    public static final String INPUT_LAST_NAME = "input[placeholder='Last Name']";
+    public static final String INPUT_ADDRESS = "textarea[ng-model='Adress']";
+    public static final String INPUT_EMAIL = "input[type='email']";
+    public static final String INPUT_PHONE = "input[type='tel']";
+    public static final String RADIO_GENDER_MALE = "input[value='Male']";
+    public static final String CHECKBOXES = "input[type='checkbox']";
+    public static final String MULTISELECT_LANGUAGES = "msdd";
+    public static final String SELECT_SKILLS = "Skills";
+    public static final String SELECT_COUNTRY = "country";
+    public static final String SELECT_YEARBOX = "yearbox";
+    public static final String SELECT_MONTH = "select[placeholder='Month']";
+    public static final String SELECT_DAYBOX = "daybox";
+    public static final String INPUT_FIRST_PASSWORD = "firstpassword";
+    public static final String INPUT_SECOND_PASSWORD = "secondpassword";
+    public static final String INPUT_IMAGE = "imagesrc";
+}
