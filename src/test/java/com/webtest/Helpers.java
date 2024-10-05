bf7f7eea8ce9435b9e22b687a9e0cedf702d8473
lucas <lucassilva.uern@gmail.com>
2024-10-04 22:22:16 -0300

    feat: add steps do allure aos métodos

--- src/test/java/com/webtest/Helpers.java
+++ src/test/java/com/webtest/Helpers.java
@@ -1,5 +1,6 @@
 package com.webtest;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -21,43 +22,50 @@ public class Helpers {
         this.driver = dr;
     }
 
-    public void fillInput(By selector, String value) {
+    @Step("Digitar no campo de input o valor '{value}'")
+    void fillInput(By selector, String value) {
         WebElement element = driver.findElement(selector);
         element.sendKeys(value);
     }
 
-    public void selectRadioButton(By selector) {
+    @Step("Selecionar o gênero a partir do seletor '{selector}'")
+    void selectRadioButton(By selector) {
         WebElement radioButton = driver.findElement(selector);
         if (!radioButton.isSelected()) {
             radioButton.click();
         }
     }
 
-    public void clearRadioButton(By locator) {
+    @Step("Limpar a seleção do radiobutton")
+    void clearRadioButton(By locator) {
         WebElement radioButton = driver.findElement(locator);
         ((JavascriptExecutor) driver).executeScript("arguments[0].checked = false;", radioButton);
     }
 
-    public void clickElement(By selector) {
+    @Step("Clicar no elemento de acordo com o seletor '{selector}' passado por parâmetro")
+    void clickElement(By selector) {
         WebElement click = driver.findElement(selector);
         if (click.isDisplayed()) {
             click.click();
         }
     }
 
-    public void selectElement(By selector, String value) {
+    @Step("Selecionar o valor '{value}' no dropdown identificado através do seletor '{selector}'")
+    void selectElement(By selector, String value) {
         WebElement dropdown = driver.findElement(selector);
         Select select = new Select(dropdown);
         select.selectByVisibleText(value);
     }
 
-    public void uploadImage(String path, By selector) {
+    @Step("Fazer upload da imagem que se encontra no path '{path}'")
+    void uploadImage(String path, By selector) {
         String filePath = Paths.get(path).toAbsolutePath().toString();
         WebElement uploadElement = driver.findElement(selector);
         uploadElement.sendKeys(filePath);
     }
 
-    public void selectMultiOptions(By containerSelector, String... optionsToSelect) {
+    @Step("Selecionar múltiplas opções '{optionsToSelect}' no seletor '{containerSelector}'")
+    void selectMultiOptions(By containerSelector, String... optionsToSelect) {
         WebElement container = driver.findElement(containerSelector);
         container.click();
 
@@ -72,7 +80,8 @@ public class Helpers {
         }
     }
 
-    public void selectAllCheckboxes(By selector) {
+    @Step("Selecionar todas as checkboxes no seletor '{selector}'")
+    void selectAllCheckboxes(By selector) {
         List<WebElement> checkboxes = driver.findElements(selector);
         for (WebElement checkbox : checkboxes) {
             if (!checkbox.isSelected()) {
@@ -81,19 +90,22 @@ public class Helpers {
         }
     }
 
-    public void assertFieldIsEmpty(By locator, String errorMessage) {
+    @Step("Verificar se o campo identificado por '{locator}' está vazio")
+    void assertFieldIsEmpty(By locator, String errorMessage) {
         WebElement element = driver.findElement(locator);
         String value = element.getAttribute("value");
         assertTrue(value == null || value.isEmpty(), errorMessage);
     }
 
-    public void assertFieldContainsText(By locator, String expectedText, String errorMessage) {
+    @Step("Verificar se o campo identificado por '{locator}' contém o texto '{expectedText}'")
+    void assertFieldContainsText(By locator, String expectedText, String errorMessage) {
         WebElement element = driver.findElement(locator);
         String actualText = element.getText();
         assertTrue(actualText.contains(expectedText), errorMessage);
     }
 
-    public void assertUncheckAllCheckboxes(By selector) {
+    @Step("Desmarcar todas as checkboxes no seletor '{selector}'")
+    void assertUncheckAllCheckboxes(By selector) {
         List<WebElement> checkboxes = driver.findElements(selector);
         for (WebElement checkbox : checkboxes) {
             if (checkbox.isSelected()) {
@@ -103,18 +115,21 @@ public class Helpers {
         }
     }
 
-    public void assertMultiselectFieldIsEmpty(By selector) {
+    @Step("Verificar se o campo multiselect identificado por '{selector}' está vazio")
+    void assertMultiselectFieldIsEmpty(By selector) {
         WebElement container = driver.findElement(selector);
         String divContent = container.getText().trim();
         assertTrue(divContent.isEmpty(), "A div contém conteúdo");
     }
 
-    public void resetForm() {
+    @Step("Reseta o formulário ao clicar no botão 'register'")
+    void resetForm() {
         WebElement refreshButton = driver.findElement(By.cssSelector("button[value='Refresh']"));
         refreshButton.click();
     }
 
-    public void fillForm() {
+    @Step("Preencher o formulário")
+    void fillForm() {
         driver.get(URL_REGISTER);
 
         String firstName = "Silva";
@@ -150,7 +165,8 @@ public class Helpers {
         uploadImage("src/test/resources/test-img.jpg", By.id(Selectors.INPUT_IMAGE));
     }
 
-    public void validatesIfFormCamposWereCleaned() {
+    @Step("Validar se o formulário está limpo")
+    void validatesIfFormFieldsWereCleaned() {
         assertFieldContainsText(By.id(Selectors.SELECT_SKILLS), "Select Skills", "O campo Skills não foi resetado");
 
         assertFieldIsEmpty(By.cssSelector(Selectors.INPUT_FIRST_NAME), "O campo First Name não foi resetado");
